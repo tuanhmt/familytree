975692114dd6f55034949b8ef6a333ef84564030
Tuan Hoang <tuan.hmt@outlook.com>
2024-06-30 22:44:51 +0300

    update ui

--- web/themes/custom/familytree/js/familytree.js
+++ web/themes/custom/familytree/js/familytree.js
@@ -26,12 +26,37 @@
    */
   Drupal.behaviors.socialButtons = {
     attach: function attach(context) {
+      var targetOffset = $('.article-title').offset().top;
+      console.log(targetOffset);
       $(window).scroll(function () {
         let windowTop = $(window).scrollTop();
-        if (windowTop > 415) {
-          $('.social-sharing-block').css('top', windowTop - 300);
+        console.log(windowTop);
+        if (windowTop > targetOffset - 50) {
+          $('.social-sharing-block').css('top', windowTop - 350);
         } else {
           $('.social-sharing-block').css('top', 0);
+        }
+      });
+    }
+  };
+
+  /**
+   * Init stickyContent behavior.
+   *
+   * @type {{attach: Drupal.behaviors.stickyContent}}
+   */
+  Drupal.behaviors.stickyContent = {
+    attach: function attach(context) {
+      var targetOffset = $('.main-section').offset().top;
+      console.log(targetOffset);
+      $(window).scroll(function () {
+        let windowTop = $(window).scrollTop();
+        if (windowTop > targetOffset - 50) {
+          $('.main-sticky-left').css('top', windowTop - 300);
+          $('.main-sticky-right').css('top', windowTop - 300);
+        } else {
+          $('.main-sticky-left').css('top', 0);
+          $('.main-sticky-right').css('top', 0);
         }
       });
     }
