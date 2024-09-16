395032383e35087e1582a025a7566510abad7fe2
Tuan Hoang <tuan.hmt@outlook.com>
2024-09-16 23:00:53 +0700

    fix the widget saving

--- web/modules/custom/ftree_core/src/Controller/FamilyTreeController.php
+++ web/modules/custom/ftree_core/src/Controller/FamilyTreeController.php
@@ -5,11 +5,34 @@ declare(strict_types=1);
 namespace Drupal\ftree_core\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Returns responses for Ftree core routes.
  */
 final class FamilyTreeController extends ControllerBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $entityTypeManager;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(EntityTypeManagerInterface $entityTypeManager) {
+    $this->entityTypeManager = $entityTypeManager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity_type.manager')
+    );
+  }
 
   /**
    * Builds the response.
@@ -26,57 +49,34 @@ final class FamilyTreeController extends ControllerBase {
       ],
     ];
 
-    $data = [
-      [
-        "id" => "dyTpfj6sr",
-        "gender" => "male",
-        "spouses" => [
-          [
-            "id" => "dyTpfj6ss",
-            "type" => "married",
-          ],
-        ],
-        "siblings" => [],
-        "parents" => [],
-        "children" => [
-          [
-            "id" => "ahfR5lR2s",
-            "type" => "blood",
-          ],
-        ],
-      ],
-      [
-        "id" => "dyTpfj6ss",
-        "gender" => "female",
-        "spouses" => [
-          [
-            "id" => "dyTpfj6sr",
-            "type" => "married",
-          ],
-        ],
-        "siblings" => [],
-        "parents" => [],
-        "children" => [
-          [
-            "id" => "ahfR5lR2s",
-            "type" => "blood",
-          ],
-        ],
-      ],
-      [
-        "id" => "ahfR5lR2s",
-        "gender" => "female",
-        "spouses" => [],
-        "siblings" => [],
-        "parents" => [
-          [
-            "id" => "dyTpfj6sr",
-            "type" => "blood",
-          ],
-        ],
-        "children" => [],
-      ],
-    ];
+    $nodeStorage = $this->entityTypeManager()->getStorage('family_node');
+
+    $ids = $nodeStorage->getQuery()
+      ->accessCheck(TRUE)
+      ->execute();
+
+    $family_nodes = $nodeStorage->loadMultiple($ids);
+    $data = [];
+    $relation_types = ["spouses", "siblings", "parents", "children"];
+    foreach ($family_nodes as $family_node) {
+      $node_data = [
+        "id" => $family_node->id(),
+        "gender" => $family_node->get('gender')?->value,
+      ];
+      foreach ($relation_types as $type) {
+        $node_data[$type] = [];
+        $relations = $family_node->get($type)->getValue();
+        foreach ($relations as $relation) {
+          $node_data[$type][] = [
+            'id' => $relation["target_id"],
+            'type' => $relation["relationship_type"],
+          ];
+        }
+      }
+
+      // Add to data collector.
+      $data[] = $node_data;
+    }
 
     $build['#attached']['drupalSettings']['ftree_nodes'] = $data;
     return $build;
