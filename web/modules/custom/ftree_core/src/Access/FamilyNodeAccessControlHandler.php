84c46f2e2f3ad9b2037a687e222290064db3a334
Tuan Hoang <tuan.hmt@outlook.com>
2024-10-05 23:47:12 +0300

    temp deny delete referenced nodes

---
+++ web/modules/custom/ftree_core/src/Access/FamilyNodeAccessControlHandler.php
@@ -0,0 +1,80 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\ftree_core\Access;
+
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Messenger\MessengerInterface;
+use Drupal\Core\Entity\EntityHandlerInterface;
+use Drupal\Core\Entity\EntityAccessControlHandler;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Access controller for the family_node entity.
+ */
+class FamilyNodeAccessControlHandler extends EntityAccessControlHandler implements EntityHandlerInterface {
+
+  /**
+   * The messenger service.
+   *
+   * @var \Drupal\Core\Messenger\MessengerInterface
+   */
+  protected $messenger;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function createInstance(ContainerInterface $container, EntityTypeInterface $entity_type) {
+    return new static(
+      $entity_type,
+      $container->get('messenger')
+    );
+  }
+
+  /**
+   * Constructs the custom access control handler instance.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeInterface $entity_type
+   *   The entity type definition.
+   * @param \Drupal\Core\Messenger\MessengerInterface $messenger
+   *   The messenger service.
+   */
+  public function __construct(EntityTypeInterface $entity_type, MessengerInterface $messenger) {
+    parent::__construct($entity_type);
+    $this->messenger = $messenger;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function checkAccess(EntityInterface $entity, $operation, AccountInterface $account) {
+    if ($operation === 'delete' && $entity->getEntityTypeId() === 'family_node') {
+      // Check if other family_node entities reference this entity.
+      $referencing_entities = _ftree_core_get_referencing_entities($entity);
+
+      if (!empty($referencing_entities)) {
+        // Deny access to delete and show the user a message.
+        // $this->messenger->addError($this->t('The Family Node entity with ID @id cannot be deleted because it is referenced by other entities.', ['@id' => $entity->id()]));
+
+        // // Add more information about the referencing entities in the message.
+        // foreach ($referencing_entities as $referencing_entity) {
+        //   $this->messenger->addError($this->t('<a href="@edit_url" target="_blank">@title (ID: @id)</a>', [
+        //     '@edit_url' => $referencing_entity->toUrl('edit-form')->toString(),
+        //     '@title' => $referencing_entity->label(),
+        //     '@id' => $referencing_entity->id(),
+        //   ]));
+        // }
+
+        return AccessResult::forbidden();
+      }
+    }
+
+    // Call the parent method for other operations.
+    return parent::checkAccess($entity, $operation, $account);
+  }
+
+}
